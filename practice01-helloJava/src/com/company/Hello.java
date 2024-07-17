8750787ecd38af93abe8ac26a2036d4dafc0942c
felix <felixzxy29@gmail.com>
2024-07-17 22:48:58 +0800

    practice 8 basic data type

--- practice01-helloJava/src/com/company/Hello.java
+++ practice01-helloJava/src/com/company/Hello.java
@@ -7,10 +7,9 @@ import java.util.ArrayList;
  * @since 2024.7.17
  * @version 1.0
  * {@code
- *
- *
  * }
- *
+ * int, float, double, boolean, char, byte, short long
+ * int, byte, short, long, float, double, boolean, char
  */
 public class Hello {
     public static void main (String[] args) {
@@ -19,21 +18,18 @@ public class Hello {
 //            Hello.sayHello();
 //            count++;
 //        } while (count < 100);
-
-
         /*
         * the multiple line comment
         * used for comment more than one line
         * */
-
         int number1 = 100;
         int number2 = 200;
         System.out.println("sum = " + sum(number1, number2));
-
     }
 
     /**
      * print Hello! when invoke this method
+     * int, short, long, byte, float, double, boolean, char
      */
     public static void sayHello () {
         System.out.println("Hello!");
---
+++ practice02-basicKnowledge/src/com/company/BasicKnowledge.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class BasicKnowledge {
+    public static void main (String[] args) {
+        int numberInt = 1;
+        short numberShort = 1;
+        long numberLong = 1L;
+        byte numberByte = 1;
+        float numberFloat = 1.1f;
+        double numberDouble = 1.2d;
+        boolean tag = true;
+        char character = 'a';
+
+        System.out.println(numberInt);
+        System.out.println(numberShort);
+        System.out.println(numberLong);
+        System.out.println(numberByte);
+        System.out.println(numberFloat);
+        System.out.println(numberDouble);
+        System.out.println(tag);
+        System.out.println(character);
+    }
+}
