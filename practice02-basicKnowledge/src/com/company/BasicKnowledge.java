cbe8b7bc7e9506d19e04d46dda6e13de48bddda1
felix <felixzxy29@gmail.com>
2024-07-17 23:09:28 +0800

    practice basic knowledge of java

--- practice02-basicKnowledge/src/com/company/BasicKnowledge.java
+++ practice02-basicKnowledge/src/com/company/BasicKnowledge.java
@@ -2,7 +2,8 @@ package com.company;
 
 public class BasicKnowledge {
     public static void main (String[] args) {
-        int numberInt = 1;
+        int numberInt1 = 3;
+        int numberInt2 = 2;
         short numberShort = 1;
         long numberLong = 1L;
         byte numberByte = 1;
@@ -11,13 +12,63 @@ public class BasicKnowledge {
         boolean tag = true;
         char character = 'a';
 
-        System.out.println(numberInt);
-        System.out.println(numberShort);
-        System.out.println(numberLong);
-        System.out.println(numberByte);
-        System.out.println(numberFloat);
-        System.out.println(numberDouble);
-        System.out.println(tag);
-        System.out.println(character);
+//        System.out.println(numberInt1);
+//        System.out.println(numberShort);
+//        System.out.println(numberLong);
+//        System.out.println(numberByte);
+//        System.out.println(numberFloat);
+//        System.out.println(numberDouble);
+//        System.out.println(tag);
+//        System.out.println(character);
+
+//        System.out.println(numberInt1 - numberInt2);
+//        System.out.println(numberInt1 + numberInt2);
+//        System.out.println(numberInt1 * numberInt2);
+//        System.out.println(numberInt1/numberInt2);
+//        System.out.println(numberInt1%numberInt2);
+
+//        if (numberInt1 > numberInt2) {
+//            System.out.println(numberInt1 + " is larger than " + numberInt2);
+//        } else if (numberInt1 < numberInt2) {
+//            System.out.println(numberInt1 + " is smaller than " + numberInt2);
+//        } else {
+//            System.out.println(numberInt1 + " equals to " + numberInt2);
+//        }
+
+        int[] nums = {1, 2, 4, 5};
+        int length = nums.length;
+
+//        for (int index = 0; index < length; index++) {
+//            System.out.println(nums[index]);
+//        }
+
+//        for (int ele : nums) {
+//            System.out.println(ele);
+//        }
+
+//        int index = 0;
+//        while (index < length) {
+//            System.out.println(nums[index]);
+//            index++;
+//        }
+
+//        int index = 0;
+//        do {
+//            System.out.println(nums[index]);
+//            index++;
+//        } while (index < length);
+
+        int number = 4;
+        switch (number) {
+            case 1:
+                System.out.println("number is 1");
+                break;
+            case 2:
+                System.out.println("number is 2");
+                break;
+
+            default:
+                System.out.println("number is 0");
+        }
     }
 }
