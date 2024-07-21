46656acd24062d6bb0aa66c4dab38d4aa142d51b
felix <felixzxy29@gmail.com>
2024-07-21 21:25:35 +0800

    practice switch/case

---
+++ practice05-switchCase/src/com/company/SwitchCasePractice.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class SwitchCasePractice {
+    public static void main (String[] args) {
+        int code = 4;
+
+        switch (code) {
+            case 0:
+                System.out.println("the code is: 0");
+                break;
+            case 1:
+                System.out.println("the code is: 1");
+                break;
+            case 2:
+                System.out.println("the code is: 2");
+                break;
+            default:
+                System.out.println("there is no code！");
+
+        }
+
+    }
+}
