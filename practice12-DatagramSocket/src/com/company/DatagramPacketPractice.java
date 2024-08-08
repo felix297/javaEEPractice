a884529c97241c5332ffd7bf409eef2c88652248
felix <felixzxy29@gmail.com>
2024-08-08 22:16:40 +0800

    practice the constructor of DatagramSocket

---
+++ practice12-DatagramSocket/src/com/company/DatagramPacketPractice.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.net.*;
+
+public class DatagramPacketPractice {
+    public static void main (String[] args) throws SocketException, UnknownHostException {
+//        1. constructor
+//        DatagramSocket socket = new DatagramSocket();
+//        System.out.println(socket.getLocalSocketAddress());
+
+//        DatagramSocket socket = new DatagramSocket(9999);
+//        System.out.println(socket.getLocalSocketAddress());
+
+//        InetAddress local = InetAddress.getByName("localhost");
+//        DatagramSocket socket = new DatagramSocket(9999, local);
+//        System.out.println(socket.getLocalSocketAddress());
+//        System.out.println(socket.getInetAddress());
+
+//        InetSocketAddress local = new InetSocketAddress("localhost", 9999);
+//        DatagramSocket socket = new DatagramSocket (local);
+//        System.out.println(socket.getLocalSocketAddress());
+
+//        DatagramSocket socket = new DatagramSocket(null);
+//        InetSocketAddress local = new InetSocketAddress("localhost", 9999);
+//        socket.bind(local);
+//        System.out.println(socket.getLocalSocketAddress());
+
+
+    }
+}
